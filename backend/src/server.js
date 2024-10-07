366e76662d68a91e28911f571ee069b914b9cb8d
Rafael Travado Fernández <rafaeltravadofernandez@MacBook-Air-de-Rafael.local>
2024-10-07 19:31:12 +0200

    Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
