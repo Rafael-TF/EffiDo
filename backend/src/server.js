730395db87439770b5f906ecb3572b73d86a264a
Rafael-TF <rafa_trafeg@hotmail.com>
2025-03-06 09:14:01 +0100

    cambios en el json

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(express.json());
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
 
